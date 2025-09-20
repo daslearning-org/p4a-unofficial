ad58b11f4f05a1fc99aabac14fadcaddd692e8fa
Somnath Das <somnath.das@daslearning.in>
2025-09-20 14:24:16 +0530

    fallback to pyjnius old version of tts app

--- pythonforandroid/recipes/flask/__init__.py
+++ pythonforandroid/recipes/flask/__init__.py
@@ -1,10 +1,17 @@
-from pythonforandroid.recipe import PyProjectRecipe
+
+from pythonforandroid.recipe import PythonRecipe
 
 
-class FlaskRecipe(PyProjectRecipe):
-    version = '3.1.1'
+class FlaskRecipe(PythonRecipe):
+    version = '2.0.3'
     url = 'https://github.com/pallets/flask/archive/{version}.zip'
-    python_depends = ['jinja2', 'werkzeug', 'markupsafe', 'itsdangerous', 'click', 'blinker']
+
+    depends = ['setuptools']
+
+    python_depends = ['jinja2', 'werkzeug', 'markupsafe', 'itsdangerous', 'click']
+
+    call_hostpython_via_targetpython = False
+    install_in_hostpython = False
 
 
 recipe = FlaskRecipe()
