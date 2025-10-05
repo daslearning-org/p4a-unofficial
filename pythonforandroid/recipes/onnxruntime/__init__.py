452861befbc67185cd2bc4a60fbeb8e351105be3
Somnath Das <somnath.das@daslearning.in>
2025-10-05 17:41:59 +0530

    removing the bfloat16 patch for ndk 28c

--- pythonforandroid/recipes/onnxruntime/__init__.py
+++ pythonforandroid/recipes/onnxruntime/__init__.py
@@ -11,7 +11,7 @@ class OnnxRuntimeRecipe(PyProjectRecipe):
     depends = ["setuptools", "wheel", "numpy", "protobuf", "pybind11"]
     patches = [
         'patches/onnx_numpy.patch',
-        'patches/mlasi_bfloat.patch',
+        #'patches/mlasi_bfloat.patch',
     ]
     build_in_src = True
 
