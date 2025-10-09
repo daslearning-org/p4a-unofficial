9ccd0d9f7dc64725e59dd23ac93d2fff0a50a49e
Somnath Das <somnath.das@daslearning.in>
2025-10-09 12:42:30 +0530

    adding tokenizers recipe

---
+++ pythonforandroid/recipes/tokenizers/__init__.py
@@ -0,0 +1,18 @@
+from pythonforandroid.recipe import RustCompiledComponentsRecipe
+
+class TokenizersRecipe(RustCompiledComponentsRecipe):
+    # The name of the package
+    name = 'tokenizers'
+
+    # Use a specific, recent version from their GitHub releases
+    version = '0.22.1'
+
+    # The URL to download the source code tarball
+    url = 'https://github.com/huggingface/tokenizers/archive/refs/tags/v{version}.tar.gz'
+    sha512sum = "1a25ee6b218232112f10bc1e082e71ed1960ab7fa62c7341b38cc5f5dc0a735cf35b3e90d4a1c1cc0aedb7c198182c2e559662459a668a2fef4e633096a1cccc"
+
+    # List of other recipes this one depends on
+    depends = [] # will use only local files as we are skipping huggingface_hub
+
+# Boilerplate to register the recipe with python-for-android
+recipe = TokenizersRecipe()
