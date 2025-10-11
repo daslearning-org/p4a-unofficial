6946f2978d75e9fc4a008e24149cfd53cba502a3
Somnath Das <somnath.das@daslearning.in>
2025-10-11 16:41:55 +0530

    fixing the requried version

--- pythonforandroid/recipes/transformers/__init__.py
+++ pythonforandroid/recipes/transformers/__init__.py
@@ -1,7 +1,7 @@
 from pythonforandroid.recipe import PyProjectRecipe
 
 class TransformersRecipe(PyProjectRecipe):
-    version = '4.57.1'  # Replace with the exact version you want to build
+    version = '4.57.0'  # Replace with the exact version you want to build
     url = 'https://github.com/huggingface/transformers/archive/refs/tags/v{version}.tar.gz'
 
     # Dependencies to include (excluding huggingface_hub, safetensors, tokenizers, etc.)
