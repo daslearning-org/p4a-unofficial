f830ca8cc4872cd9449b3f4a27922629195fec71
Somnath Das <somnath.das@daslearning.in>
2026-03-14 13:38:47 +0530

    adding extra element for xml Application tag

--- pythonforandroid/bootstraps/common/build/build.py
+++ pythonforandroid/bootstraps/common/build/build.py
@@ -974,6 +974,11 @@ tools directory of the Android SDK.
     ap.add_argument('--extra-manifest-application-arguments', default='',
                     help='Extra arguments to be added to the <manifest><application> tag of'
                          'AndroidManifest.xml')
+    ap.add_argument(
+        '--extra-manifest-application-element',
+        default='',
+        help='Extra xml element to put into <application> tag, example: a <service> tag'
+    )
     ap.add_argument('--manifest-placeholders', dest='manifest_placeholders',
                     default='[:]', help=('Inject build variables into the manifest '
                                          'via the manifestPlaceholders property'))
